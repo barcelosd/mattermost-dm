1fa74c3fabe104c817ce06ddf32caea269df6ec3
barcelosd <barcelosd@gmail.com>
2026-05-13 15:58:52 -0300

    ajuste

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 
-const app = express();
 app.use(express.json({ type: '*/*' }));
 
 app.use((err, req, res, next) => {
