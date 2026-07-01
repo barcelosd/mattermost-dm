d314546a86799572b2d2f1ca22b67806739880eb
barcelosd <barcelosd@gmail.com>
2026-07-01 16:25:07 -0300

    logs_sp_br

--- app.js
+++ app.js
@@ -1727,7 +1727,7 @@ async function pollingRedmineIssues() {
     const currentRunTimestamp = Date.now();
 
     //console.log(`[Polling] Buscando tarefas alteradas desde: ${new Date(lastPollingTimestamp).toISOString()}`);
-    console.log(`[Polling] Buscando tarefas alteradas desde: ${dayjs(lastPollingTimestamp).format('YYYY-MM-DD HH:mm:ss')}`);
+    console.log(`[Polling] Buscando tarefas alteradas desde: ${dayjs(lastPollingTimestamp).tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss')}`);
 
     // 2. Faz a chamada da API do Redmine (ajuste conforme o seu código atual)
     const issues = await fetchRecentIssues(); 
